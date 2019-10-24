b01b8d938a220234b8d06a37a6858cbc6af75fd8
Zavier Yohe <ZYohe@uttyler.edu>
2019-10-24 17:48:59 -0500

    Added digital movement, fixed pin addressing
    Use option to switch between digital and analog mode.

--- Theseus_Mega_Controller/Theseus_Mega_Controller.ino
+++ Theseus_Mega_Controller/Theseus_Mega_Controller.ino
@@ -1,9 +1,3 @@
-/*
- Example sketch for the PS4 Bluetooth library - developed by Kristian Lauszus
- For more information visit my blog: http://blog.tkjelectronics.dk/ or
- send me an e-mail:  kristianl@tkjelectronics.com
- */
-
 #include <PS4BT.h>
 #include <usbhub.h>
 
@@ -16,15 +10,18 @@
 #define DEADZONE 13
 #define STICK_CENTER 127
 
-#define PIN_FRONT_LEFT_DIR  74
-#define PIN_FRONT_RIGHT_DIR 78
-#define PIN_BACK_LEFT_DIR   72
-#define PIN_BACK_RIGHT_DIR  76
-#define PIN_FRONT_LEFT_PWM  5
-#define PIN_FRONT_RIGHT_PWM 16
-#define PIN_BACK_LEFT_PWM   7
-#define PIN_BACK_RIGHT_PWM  15
+#define PIN_FRONT_LEFT_DIR  26
+#define PIN_FRONT_RIGHT_DIR 22
+#define PIN_BACK_LEFT_DIR   28
+#define PIN_BACK_RIGHT_DIR  24
+#define PIN_FRONT_LEFT_PWM  2
+#define PIN_FRONT_RIGHT_PWM 3
+#define PIN_BACK_LEFT_PWM   4
+#define PIN_BACK_RIGHT_PWM  5
 
+#define DIGITAL_SPEED 127
+
+// Magic bluetooth setup stuff
 USB Usb;
 //USBHub Hub1(&Usb); // Some dongles have a hub inside
 BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
@@ -39,9 +36,12 @@ PS4BT PS4(&Btd, PAIR);
 
 bool printAngle, printTouch;
 uint8_t oldL2Value, oldR2Value;
+// Default to digital mode
+bool analogMode = false;
 
 void setup() 
 {
+  //More magic bluetooth stuff
   Serial.begin(115200);
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
@@ -52,42 +52,102 @@ void setup()
     while (1); // Halt
   }
   Serial.print(F("\r\nPS4 Bluetooth Library Started"));
+
+  // Put all pins we care about into the right mode)
+  pinMode(PIN_FRONT_LEFT_DIR, OUTPUT);
+  pinMode(PIN_BACK_LEFT_DIR, OUTPUT);
+  pinMode(PIN_FRONT_RIGHT_DIR, OUTPUT);
+  pinMode(PIN_BACK_RIGHT_DIR, OUTPUT);
+  pinMode(PIN_FRONT_LEFT_PWM, OUTPUT);
+  pinMode(PIN_BACK_LEFT_PWM, OUTPUT);
+  pinMode(PIN_FRONT_RIGHT_PWM, OUTPUT);
+  pinMode(PIN_BACK_RIGHT_PWM, OUTPUT);
 }
 
 void loop() 
 {
   Usb.Task();
-
   if (PS4.connected()) 
   {
-
-    uint8_t leftStickX = PS4.getAnalogHat(LeftHatX);
-    uint8_t leftStickY = PS4.getAnalogHat(LeftHatY);
-    uint8_t rightStickX = PS4.getAnalogHat(RightHatX);
-    uint8_t rightStickY = PS4.getAnalogHat(RightHatY);
-
-    uint8_t leftDeadzoneX = checkInDeadzone(leftStickX);
-    uint8_t leftDeadzoneY = checkInDeadzone(leftStickY);
-    uint8_t rightDeadzoneX = checkInDeadzone(rightStickX);
-    uint8_t rightDeadzoneY = checkInDeadzone(rightStickY);
-
-    if (leftDeadzoneY != 1 && leftDeadzoneX == 1 && rightDeadzoneX == 1)
+    if(PS4.getButtonClick(OPTIONS))
     {
-      boolean dir = (leftDeadzoneY == 2 ? true : false);
-      uint8_t normalizedPower = (leftDeadzoneY == 2 ? leftStickY : abs(leftStickY - STICK_CENTER));
-      moveRobot(dir, normalizedPower, dir, normalizedPower, dir, normalizedPower, dir, normalizedPower);
+      analogMode = !analogMode;
     }
 
-    if (leftDeadzoneY == 1 && leftDeadzoneX == 0 && rightDeadzoneX == 1)
+    if(analogMode)
     {
-      uint8_t normalizedPower = (leftDeadzoneX == 2 ? leftStickX : abs(leftStickX - STICK_CENTER));
-      moveRobot(false, normalizedPower, true, normalizedPower, false, 0, false, 0);
+      int leftStickX = PS4.getAnalogHat(LeftHatX);
+      int leftStickY = PS4.getAnalogHat(LeftHatY);
+      int rightStickX = PS4.getAnalogHat(RightHatX);
+      int rightStickY = PS4.getAnalogHat(RightHatY);
+  
+      int leftDeadzoneX = checkInDeadzone(leftStickX);
+      int leftDeadzoneY = checkInDeadzone(leftStickY);
+      int rightDeadzoneX = checkInDeadzone(rightStickX);
+      int rightDeadzoneY = checkInDeadzone(rightStickY);
+  
+      if (leftDeadzoneY != 1 && leftDeadzoneX == 1 && rightDeadzoneX == 1)
+      {
+        boolean dir = (leftDeadzoneY == 2 ? true : false);
+        int normalizedPower = (leftDeadzoneY == 2 ? leftStickY : abs(leftStickY - STICK_CENTER));
+        moveRobot(dir, normalizedPower, dir, normalizedPower, dir, normalizedPower, dir, normalizedPower);
+      }
+  
+      if (leftDeadzoneY == 1 && leftDeadzoneX == 0 && rightDeadzoneX == 1)
+      {
+        int normalizedPower = (leftDeadzoneX == 2 ? leftStickX : abs(leftStickX - STICK_CENTER));
+        moveRobot(false, normalizedPower, true, normalizedPower, false, 0, false, 0);
+      }
+  
+      if (leftDeadzoneY == 1 && leftDeadzoneX == 0 && rightDeadzoneX == 1)
+      {
+        int normalizedPower = (leftDeadzoneX == 2 ? leftStickX : abs(leftStickX - STICK_CENTER));
+        moveRobot(false, 0, false, 0, false, normalizedPower, true, normalizedPower);
+      }
     }
 
-    if (leftDeadzoneY == 1 && leftDeadzoneX == 0 && rightDeadzoneX == 1)
+    else
     {
-      uint8_t normalizedPower = (leftDeadzoneX == 2 ? leftStickX : abs(leftStickX - STICK_CENTER));
-      moveRobot(false, 0, false, 0, false, normalizedPower, true, normalizedPower);
+      bool forward = PS4.getButtonPress(UP);
+      bool backward = PS4.getButtonPress(DOWN);
+      bool strafeLeft = PS4.getButtonPress(LEFT);
+      bool strafeRight = PS4.getButtonPress(RIGHT);
+      bool turnLeft = PS4.getButtonPress(CIRCLE);
+      bool turnRight = PS4.getButtonPress(SQUARE);
+
+      if (forward)
+      {
+        moveRobot(true, DIGITAL_SPEED, true, DIGITAL_SPEED, true, DIGITAL_SPEED, true, DIGITAL_SPEED);
+      }
+
+      else if(strafeLeft)
+      {
+        moveRobot(false, DIGITAL_SPEED, true, DIGITAL_SPEED, false, 0, false, 0);
+      }
+
+      else if(strafeRight)
+      {
+        moveRobot(false, 0, false, 0, false, DIGITAL_SPEED, true, DIGITAL_SPEED);
+      }
+
+      else if(backward)
+      {
+        moveRobot(false, DIGITAL_SPEED, false, DIGITAL_SPEED, false, DIGITAL_SPEED, false, DIGITAL_SPEED);
+      }
+
+      else if(turnLeft)
+      {
+        moveRobot(false, DIGITAL_SPEED, false, DIGITAL_SPEED, true, DIGITAL_SPEED, true, DIGITAL_SPEED);
+      }
+
+      else if(turnRight)
+      {
+        moveRobot(true, DIGITAL_SPEED,  true, DIGITAL_SPEED, false, DIGITAL_SPEED, false, DIGITAL_SPEED);
+      }
+      else
+      {
+        moveRobot(false, 0, false, 0, false, 0, false, 0);
+      }
     }
   }
 }
@@ -96,7 +156,7 @@ void loop()
 // Value of 0 means it is out of the deadzone, in the negative direction
 // Value of 2 means it is out of the deadzone, in the positive direction
 // value of 1 means it is in the deadzone
-uint8_t checkInDeadzone(uint8_t value)
+int checkInDeadzone(int value)
 {
   if (value < STICK_CENTER - DEADZONE)
   {
@@ -117,7 +177,7 @@ uint8_t checkInDeadzone(uint8_t value)
 // Clamps value between minValue and maxValue
 // Has no effect on values between the two
 // Value may be any int type, minValue and maxValue MUST be 8-bit
-uint8_t clampInt(int value, uint8_t minValue, uint8_t maxValue)
+int clampInt(int value, int minValue, int maxValue)
 {
   if (value < minValue)
   {
@@ -139,13 +199,13 @@ void moveRobot(boolean dirFrontL, int powerFrontL, boolean dirBackL, int powerBa
 {
   digitalWrite(PIN_FRONT_LEFT_DIR, dirFrontL);
   digitalWrite(PIN_BACK_LEFT_DIR, dirBackL);
-  digitalWrite(PIN_FRONT_RIGHT_DIR, dirFrontR);
-  digitalWrite(PIN_BACK_RIGHT_DIR, dirBackR);
+  digitalWrite(PIN_FRONT_RIGHT_DIR, !dirFrontR);
+  digitalWrite(PIN_BACK_RIGHT_DIR, !dirBackR);
 
-  uint8_t normalizedFrontL = clampInt(powerFrontL * 2, 0, 255);
-  uint8_t normalizedBackL = clampInt(powerBackL * 2, 0, 255);
-  uint8_t normalizedFrontR = clampInt(powerFrontR * 2, 0, 255);
-  uint8_t normalizedBackR = clampInt(powerBackR * 2, 0, 255);
+  int normalizedFrontL = clampInt(powerFrontL * 2, 0, 255);
+  int normalizedBackL = clampInt(powerBackL * 2, 0, 255);
+  int normalizedFrontR = clampInt(powerFrontR * 2, 0, 255);
+  int normalizedBackR = clampInt(powerBackR * 2, 0, 255);
   
   analogWrite(PIN_FRONT_LEFT_PWM, normalizedFrontL);
   analogWrite(PIN_BACK_LEFT_PWM, normalizedBackL);
